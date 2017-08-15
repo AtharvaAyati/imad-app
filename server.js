96e1dafd35e2d05927f9f344b65db0ec141001e5
AtharvaAyati <30552043+AtharvaAyati@users.noreply.github.com>
2017-08-15 23:32:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article one',function(req,res) {
+    res.send('article one request is served');
+});
+
+app.get('/article two',function(req,res) {
+    res.send('article two request is served');
+});
+
+app.get('/article three',function(req,res) {
+    res.send('article three request is served');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
